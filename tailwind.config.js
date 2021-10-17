15a4f8b158bf0af95d186bbc5dcfab651624eecd
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-17 12:36:57 -0400

    💄 <style> Update font to metal mania

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    fontFamily: {
+      'metal-mania': ['Metal Mania', 'sans-serif'],
+    },
   },
   variants: {
     extend: {},
