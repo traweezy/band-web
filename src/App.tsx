8b9bd719dcd7789f74fc3d0ea4a37a45602e4f9e
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-17 13:00:07 -0400

    ✨ <feat> set up placeholder page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import Placeholder from './assets/placeholder.gif';
 
 const App = () => (
   <div className="h-full flex items-center w-full">
-    <div className="mx-auto">
-      <div className="text-4xl text-center">
-        [Name Redacted]&apos;s website
-        <br />
-        under construction
-        <img src={Placeholder} alt="placeholder" />
+    <div className="mx-auto text-center">
+      <div className="text-6xl mb-10">[Name Redacted]</div>
+      <div className="text-4xl mb-5">under construction</div>
+      <div>
+        <img className="rounded-md" src={Placeholder} alt="placeholder" />
       </div>
     </div>
   </div>
