802cc1ca41106d02610ed3aa2f1be56d84eec50a
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-17 14:10:28 -0400

    ✨ <feat> Update band name copy

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Placeholder from './assets/placeholder.gif';
 const App = () => (
   <div className="flex items-center w-full h-full">
     <div className="mx-auto text-center">
-      <div className="mb-10 text-6xl">[Name Redacted]</div>
-      <div className="mb-5 text-4xl">under construction</div>
+      <div className="mb-10 text-6xl">Envoys</div>
+      <div className="mb-5 text-4xl">[under construction]</div>
       <div>
         <img className="md:rounded-md" src={Placeholder} alt="placeholder" />
       </div>
